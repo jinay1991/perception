dc08c8b261487a77ef2f9c1a0b02ebac18fb57ec
Jinay Patel <jinay1991@gmail.com>
2020-08-23 16:23:27 +0200

    Rename files

---
+++ perception/inference_engine/test/inference_engine_tests.cpp
@@ -0,0 +1,98 @@
+///
+/// @file
+/// @copyright Copyright (c) 2020. MIT License
+///
+#include "perception/inference_engine/i_inference_engine.h"
+#include "perception/inference_engine/inference_engine_strategy.h"
+#include "perception/inference_engine/tf_inference_engine.h"
+#include "perception/inference_engine/tflite_inference_engine.h"
+#include "perception/inference_engine/torch_inference_engine.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/videoio.hpp>
+
+#include <cstdint>
+#include <memory>
+#include <string>
+
+namespace perception
+{
+namespace
+{
+template <typename T>
+class InferenceEngineFixture_WithInferenceEngineType : public ::testing::Test
+{
+  public:
+    InferenceEngineFixture_WithInferenceEngineType()
+        : test_image_path_{"data/grace_hopper.jpg"},
+          test_image_{cv::imread(test_image_path_, cv::IMREAD_UNCHANGED)},
+          inference_engine_parameters_{"external/ssd_mobilenet_v2_coco/saved_model",
+                                       "image_tensor",
+                                       {"detection_classes", "detection_scores", "detection_boxes"}},
+          unit_{std::make_unique<T>(inference_engine_parameters_)}
+    {
+    }
+
+  protected:
+    void SetUp() override { unit_->Init(); }
+    void RunOnce() { unit_->Execute(test_image_); }
+    void TearDown() override { unit_->Shutdown(); }
+
+    std::vector<cv::Mat> GetInferenceResults() const { return unit_->GetResults(); }
+    InferenceEngineParameters GetInferenceParameters() const { return inference_engine_parameters_; }
+
+  private:
+    const std::string test_image_path_;
+    const Image test_image_;
+    const InferenceEngineParameters inference_engine_parameters_;
+    InferenceEnginePtr unit_;
+};
+TYPED_TEST_SUITE_P(InferenceEngineFixture_WithInferenceEngineType);
+
+TYPED_TEST_P(InferenceEngineFixture_WithInferenceEngineType, InferenceEngine_GivenTypicalInputs_ExpectInferenceResults)
+{
+    // When
+    this->RunOnce();
+
+    // Then
+    const auto actual = this->GetInferenceResults();
+    EXPECT_EQ(this->GetInferenceParameters().output_tensor_names.size(), actual.size());
+}
+
+REGISTER_TYPED_TEST_SUITE_P(InferenceEngineFixture_WithInferenceEngineType,
+                            InferenceEngine_GivenTypicalInputs_ExpectInferenceResults);
+
+typedef ::testing::Types<TFInferenceEngine /* , TFLiteInferenceEngine , TorchInferenceEngine*/>
+    InferenceEngineTestTypes;
+INSTANTIATE_TYPED_TEST_SUITE_P(InferenceEngine,
+                               InferenceEngineFixture_WithInferenceEngineType,
+                               InferenceEngineTestTypes);
+
+//////////////////////
+
+class InferenceEngineStrategyTest : public ::testing::TestWithParam<InferenceEngineType>
+{
+  public:
+    InferenceEngineStrategyTest() : inference_engine_params_{}, unit_{} {}
+
+  protected:
+    InferenceEngineParameters inference_engine_params_;
+    InferenceEngineStrategy unit_;
+};
+TEST_P(InferenceEngineStrategyTest, GivenInferenceEngine_ExpectSelectedEngine)
+{
+
+    unit_.SelectInferenceEngine(GetParam(), inference_engine_params_);
+
+    EXPECT_EQ(GetParam(), unit_.GetInferenceEngineType());
+}
+INSTANTIATE_TEST_CASE_P(InferenceEngine,
+                        InferenceEngineStrategyTest,
+                        ::testing::Values(InferenceEngineType::kTensorFlow,
+                                          InferenceEngineType::kTensorFlowLite,
+                                          InferenceEngineType::kTorch));
+}  // namespace
+}  // namespace perception
---
+++ perception/lane/test/lane_node_tests.cpp
@@ -0,0 +1,36 @@
+///
+/// @file
+/// @copyright Copyright (c) 2020. MIT License
+///
+#include "perception/lane/node/lane_node.h"
+
+#include "middleware/communication/intra_process_pub_sub_factory.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace perception
+{
+namespace
+{
+class LaneNodeTest : public ::testing::Test
+{
+  public:
+    LaneNodeTest() : factory_{}, unit_{factory_} {}
+
+  protected:
+    void SetUp() override { unit_.Init(); }
+    void RunOnce() { unit_.ExecuteStep(); }
+    void TearDown() override { unit_.Shutdown(); }
+
+  private:
+    middleware::IntraProcessPubSubFactory factory_;
+    LaneNode unit_;
+};
+
+TEST_F(LaneNodeTest, GivenTypicalInput_ExpectValidLaneDetected)
+{
+    RunOnce();
+}
+}  // namespace
+}  // namespace perception

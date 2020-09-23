be119abf2591950aefead98fa901435856b60da0
Jinay Patel <jinay1991@gmail.com>
2020-09-23 21:39:53 +0000

    Add Test Network to keep libtorch in check

--- perception/inference_engine/torch_inference_engine.cpp
+++ perception/inference_engine/torch_inference_engine.cpp
@@ -10,9 +10,32 @@
 
 namespace perception
 {
+namespace
+{
+class TestNet : public torch::nn::Module
+{
+  public:
+    explicit Net(const std::int32_t n, const std::int32_t m)
+    {
+        weight = register_parameter("weight", torch::randn({n, m}));
+        bias = register_parameter("bias", torch::randn(m));
+    }
+    torch::Tensor forward(const torch::Tensor& input) { return torch::addmm(bias, input, weight); }
+
+  private:
+    torch::Tensor weight;
+    torch::Tensor bias;
+};
+}  // namespace
+
 TorchInferenceEngine::TorchInferenceEngine(const InferenceEngineParameters& /* params */) : results_{} {}
 
-void TorchInferenceEngine::Init() {}
+void TorchInferenceEngine::Init()
+{
+    auto test_net = TestNet(4, 5);
+    const torch::Tensor tensor = test_net.forward(torch::ones({2, 4}));
+    std::cout << tensor << std::endl;
+}
 
 void TorchInferenceEngine::Execute(const Image& image) {}
 

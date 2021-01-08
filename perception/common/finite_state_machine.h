8da3c2b20aeefd904d63daa923392c803e3e45db
Jinay Patel <jinay1991@gmail.com>
2021-01-08 08:20:15 +0000

    Fix typo

--- perception/common/finite_state_machine.h
+++ perception/common/finite_state_machine.h
@@ -77,7 +77,7 @@ class FiniteStateMachine final
                                           const Action& state_action = nullptr,
                                           const Action& exit_action = nullptr) noexcept
     {
-        ReplaceCurrentActionssIfRelevant(state, entry_action, state_action, exit_action);
+        ReplaceCurrentActionsIfRelevant(state, entry_action, state_action, exit_action);
         state_actions_.insert({state, Actions{entry_action, state_action, exit_action}});
     }
 
@@ -208,10 +208,10 @@ class FiniteStateMachine final
     /// @param entry_action [in] - State Entry action
     /// @param state_action [in] - State action
     /// @param exit_action [in] - State Exit action
-    inline constexpr void ReplaceCurrentActionssIfRelevant(const T state,
-                                                           const Action& entry_action,
-                                                           const Action& state_action,
-                                                           const Action& exit_action) noexcept
+    inline constexpr void ReplaceCurrentActionsIfRelevant(const T state,
+                                                          const Action& entry_action,
+                                                          const Action& state_action,
+                                                          const Action& exit_action) noexcept
     {
         if (current_state_ == state)
         {
